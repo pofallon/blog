462697872a918cd50d578462615e9e2e1d812a2d
Paul O'Fallon <pofallon@users.noreply.github.com>
2020-05-03 23:28:02 -0400

    Don't need return

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,14 +1,12 @@
 import React from "react"
 
-const Playlist = ({content, title}) => {
-  return (
-    <div>
-      <h3>{title || content.title}</h3>
-      <ul>
-        {content.videos.map(v => <li><a href="{v.url}">{v.title}</a></li>)}
-      </ul>
-    </div>
-  )
-}
+const Playlist = ({content, title}) =>
+  <div>
+    <h3>{title || content.title}</h3>
+    <ul>
+      {content.videos.map(v => <li><a href={v.url}>{v.title}</a></li>)}
+    </ul>
+  </div>
+
 
 export default Playlist
